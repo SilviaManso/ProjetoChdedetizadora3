01ae076f1a1b2aec43c6d6f45083d840ae94722a
Silvia <silvia.vs.andre@gmail.com>
2019-09-26 22:31:52 -0300

    classe dao
    metodos excluir

--- src/main/java/com/projetos/projetochdedetizadora/dao/CompraDao.java
+++ src/main/java/com/projetos/projetochdedetizadora/dao/CompraDao.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.projetos.projetochdedetizadora.dao;
 
-/**
- *
- * @author Silvia Manso
- */
+import com.projetos.projetochdedetizadora.model.Compra;
+import com.projetos.projetochdedetizadora.model.Produto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+
 public class CompraDao {
+    
+     private ObservableList<Compra> obsList = FXCollections.observableArrayList();
+ 
+    public void salvar(Compra compra) {
+        try {
+            Session session = ConexaoBanco.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.merge(compra);
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Registro gravado com sucesso");
+        }catch (HibernateException erro) {
+          System.out.println("Ocorreu o erro:" + erro);
+        }  
+    }
+  
+     public void excluir(Compra compra) {
+        try {
+            Session session = ConexaoBanco.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(compra);
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Registro foi excluído com sucesso!");
+        } catch (HibernateException erro) {
+            System.out.println("Ocorreu o erro: " + erro);
+        }
+
+    }
     
 }
